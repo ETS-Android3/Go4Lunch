158f51211a820285610dd85e0af52275886bb65c
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2022-03-03 12:13:26 +0100

    initial project

---
+++ app/src/main/java/com/tonyocallimoutou/go4lunch/MainActivity.java
@@ -0,0 +1,32 @@
+package com.tonyocallimoutou.go4lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initBottomNavigationView();
+    }
+
+    private void initBottomNavigationView() {
+        BottomNavigationView navigationView = findViewById(R.id.nav_view);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_map, R.id.navigation_list, R.id.navigation_workmates)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+}
