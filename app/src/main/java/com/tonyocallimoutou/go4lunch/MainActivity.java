1ea5543ca39a424481ee3bea6bd4fb8198913ada
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2022-03-03 12:18:13 +0100

    feat : init ActionBar

--- app/src/main/java/com/tonyocallimoutou/go4lunch/MainActivity.java
+++ app/src/main/java/com/tonyocallimoutou/go4lunch/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tonyocallimoutou.go4lunch;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -7,6 +9,9 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,6 +22,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initBottomNavigationView();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu,menu);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.d("TAG", "onOptionsItemSelected: Home");
+                return true;
+            case R.id.search_menu:
+                Log.d("TAG", "onOptionsItemSelected: search");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initBottomNavigationView() {
