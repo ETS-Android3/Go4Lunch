0a90cead0ea45dc9c5ac09e63755a06b28590d82
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2022-03-21 17:07:29 +0100

    Fix : init Google Map + Permission Location

---
+++ app/src/main/java/com/tonyocallimoutou/go4lunch/ui/mapview/MapViewFragment.java
@@ -0,0 +1,155 @@
+package com.tonyocallimoutou.go4lunch.ui.mapview;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.tonyocallimoutou.go4lunch.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback{
+
+    @BindView(R.id.message_map_view)
+    LinearLayout message_map_view;
+    @BindView(R.id.fab_map_view)
+    FloatingActionButton fabMap;
+
+    SupportMapFragment mapFragment;
+    GoogleMap mGoogleMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_map_view, container, false);
+
+        ButterKnife.bind(this, view);
+
+        fabMap.setVisibility(View.INVISIBLE);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            initFragment();
+        }
+    }
+
+    // WHEN PERMISSION IS GRANTED
+
+    public void initFragment() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            message_map_view.setVisibility(View.GONE);
+            mapFragment.getMapAsync(this);
+            fabMap.setVisibility(View.VISIBLE);
+        }
+    }
+
+
+    // ON MAP READY
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getActivity());
+
+        Log.d("TAG", "onMapReady: ");
+
+        //clear old markers
+        googleMap.clear();
+
+        this.mGoogleMap = googleMap;
+    }
+
+    // INIT PERMISSION
+
+    @OnClick(R.id.button_message_map_view)
+    public void initPermissionManager() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) == true)
+        {
+            showAlertDialog();
+        }
+        else
+        {
+            askForPermission();
+        }
+    }
+
+    public void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        builder.setTitle(R.string.title_alertDialog_permission);
+        builder.setMessage(R.string.message_alertDialog_permission);
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(getContext().getResources().getString(R.string.positive_button_alertDialog_permission), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                askForPermission();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void askForPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+    }
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    initFragment();
+                }
+            });
+
+
+    // GET USER LOCATION
+
+    @OnClick(R.id.fab_map_view)
+    public void cameraOnLocation() {
+
+        mGoogleMap.getMyLocation()
+
+        LatLng userLocation = new LatLng(0, 0);
+        mGoogleMap.addMarker(new MarkerOptions().position(userLocation).title("user's marker"));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+    }
+}
