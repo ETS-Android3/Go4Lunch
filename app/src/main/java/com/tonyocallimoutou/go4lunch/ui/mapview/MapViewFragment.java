5c240d8500bf711bc2915390c21df42c9ab4cec4
Tonyo Callimoutou <tonyo.callimoutou@hotmail.fr>
2022-03-23 09:14:08 +0100

    Fix : Camera on User -> OnMapReady

--- app/src/main/java/com/tonyocallimoutou/go4lunch/ui/mapview/MapViewFragment.java
+++ app/src/main/java/com/tonyocallimoutou/go4lunch/ui/mapview/MapViewFragment.java
@@ -9,10 +9,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.location.Location;
 import android.location.LocationListener;
 import android.net.Uri;
 import android.os.Bundle;
@@ -22,22 +25,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.tonyocallimoutou.go4lunch.R;
+
+import java.util.concurrent.Executor;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MapViewFragment extends Fragment implements OnMapReadyCallback{
+public class MapViewFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, OnCompleteListener<Location>, GoogleMap.OnPoiClickListener {
 
     @BindView(R.id.message_map_view)
     LinearLayout message_map_view;
@@ -46,6 +64,17 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback{
 
     SupportMapFragment mapFragment;
     GoogleMap mGoogleMap;
+    CameraPosition cameraPosition;
+    View locationButton;
+
+    PlacesClient placesClient;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    Location userLocation;
+
+    // Bundle
+    private final String KEY_LOCATION = "KEY_LOCATION";
+    private final String KEY_CAMERA_POSITION = "KEY_CAMERA_POSITION";
+
 
     @Nullable
     @Override
@@ -54,10 +83,14 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback{
                              @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_map_view, container, false);
-
         ButterKnife.bind(this, view);
 
         fabMap.setVisibility(View.INVISIBLE);
+
+        // INIT PLACE
+        Places.initialize(getActivity().getApplicationContext(), getString(R.string.map_api_key));
+        placesClient = Places.createClient(getContext());
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
 
         return view;
     }
@@ -65,11 +98,28 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback{
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // BUNDLE
+        if (savedInstanceState != null) {
+            userLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
         mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             initFragment();
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        if (mGoogleMap != null) {
+            outState.putParcelable(KEY_CAMERA_POSITION, mGoogleMap.getCameraPosition());
+            outState.putParcelable(KEY_LOCATION, userLocation);
+        }
+        Log.d("TAG", "onSaveInstanceState: " + mGoogleMap.getCameraPosition());
+        super.onSaveInstanceState(outState);
     }
 
     // WHEN PERMISSION IS GRANTED
@@ -87,12 +137,9 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback{
 
     @OnClick(R.id.button_message_map_view)
     public void initPermissionManager() {
-        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) == true)
-        {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) == true) {
             showAlertDialog();
-        }
-        else
-        {
+        } else {
             askForPermission();
         }
     }
@@ -126,28 +173,96 @@ public class MapViewFragment extends Fragment implements OnMapReadyCallback{
                 }
             });
 
+
     // ON MAP READY
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getActivity());
 
-        Log.d("TAG", "onMapReady: ");
+        // Camera
+        if (mGoogleMap == null) {
+            getDeviceLocation();
+        }
+        else {
+            if (cameraPosition != null) {
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(cameraPosition.target, cameraPosition.zoom));
+            }
+        }
 
-        //clear old markers
-        googleMap.clear();
+        mGoogleMap = googleMap;
 
-        this.mGoogleMap = googleMap;
+
+        // Click on Marker = Restaurant
+        initMarkerRestaurant();
+        mGoogleMap.setOnMarkerClickListener(this);
+
+        // User
+        initUserLocationGoogleMap();
+
+
+        mGoogleMap.setOnPoiClickListener(this);
+
     }
 
+    // GET DEVICE LOCATION
+    @SuppressLint("MissingPermission")
+    public void getDeviceLocation() {
+        Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+
+        locationResult.addOnCompleteListener(this);
+    }
+
+    @Override
+    public void onComplete(@NonNull Task<Location> task) {
+        if (task.isSuccessful()) {
+            // Set the map's camera position to the current location of the device.
+            userLocation = task.getResult();
+            if (userLocation != null) {
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(userLocation.getLatitude(),
+                                userLocation.getLongitude()), 15));
+            }
+        }
+    }
 
     // GET USER LOCATION
 
+    @SuppressLint({"MissingPermission", "ResourceType"})
+    public void initUserLocationGoogleMap() {
+        mGoogleMap.setMyLocationEnabled(true);
+        locationButton = mapFragment.getView().findViewById(0x2);
+        if (locationButton != null) {
+            locationButton.setVisibility(View.GONE);
+        }
+    }
+
     @OnClick(R.id.fab_map_view)
     public void cameraOnLocation() {
+        Log.d("TAG", "cameraOnLocation: ");
+        locationButton.callOnClick();
+    }
 
-        LatLng userLocation = new LatLng(0, 0);
-        mGoogleMap.addMarker(new MarkerOptions().position(userLocation).title("user's marker"));
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+    // Restaurant
+
+    public void initMarkerRestaurant() {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(@NonNull Marker marker) {
+        Toast.makeText(getContext(), "Clicked: " +
+                        marker.getTitle() + "\nPlace ID:" + marker.getId(),
+                Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+
+    @Override
+    public void onPoiClick(@NonNull PointOfInterest poi) {
+        Toast.makeText(getContext(), "Clicked: " +
+                        poi.name + "\nPlace ID:" + poi.placeId,
+                Toast.LENGTH_SHORT).show();
+        Log.d("TAG", "onPoiClick: " + poi);
     }
 }
